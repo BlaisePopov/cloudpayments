5495cc8f9926abd6c64b1f12ef90292299c0cdac
Blaise Popov <blaise.popov@gmail.com>
2025-01-23 18:02:01 +0300

    docs: Добавление ссылок на документацию API в описание структур request

--- requests.go
+++ requests.go
@@ -1,6 +1,6 @@
 package cloudpayments
 
-// CryptogramPaymentRequest представляет запрос на оплату по криптограмме.
+// CryptogramPaymentRequest представляет запрос на оплату по криптограмме. [https://developers.cloudpayments.ru/#oplata-po-kriptogramme]
 type CryptogramPaymentRequest struct {
 	Amount               float64                `json:"Amount"`
 	Currency             *CurrencyCode          `json:"Currency"`
@@ -18,7 +18,7 @@ type CryptogramPaymentRequest struct {
 	RequestID            *string                `json:"-"`
 }
 
-// TokenPaymentRequest представляет запрос на оплату по токену.
+// TokenPaymentRequest представляет запрос на оплату по токену. [https://developers.cloudpayments.ru/#oplata-po-tokenu-rekarring]
 type TokenPaymentRequest struct {
 	Amount           float64                `json:"Amount"`
 	Currency         *CurrencyCode          `json:"Currency"`
@@ -35,14 +35,14 @@ type TokenPaymentRequest struct {
 	RequestID        *string                `json:"-"`
 }
 
-// Confirm3DSRequest представляет запрос на подтверждение 3DS оплаты.
+// Confirm3DSRequest представляет запрос на подтверждение 3DS оплаты. [https://developers.cloudpayments.ru/#obrabotka-3-d-secure]
 type Confirm3DSRequest struct {
 	TransactionId int64   `json:"TransactionId"`
 	PaRes         string  `json:"PaRes"`
 	RequestID     *string `json:"-"`
 }
 
-// ConfirmPaymentRequest представляет запрос на подтверждение оплаты.
+// ConfirmPaymentRequest представляет запрос на подтверждение оплаты. [https://developers.cloudpayments.ru/#podtverzhdenie-oplaty]
 type ConfirmPaymentRequest struct {
 	TransactionId int64                  `json:"TransactionId"`
 	Amount        float64                `json:"Amount"`
@@ -50,7 +50,7 @@ type ConfirmPaymentRequest struct {
 	RequestID     *string                `json:"-"`
 }
 
-// RefundPaymentRequest представляет запрос на возврат средств.
+// RefundPaymentRequest представляет запрос на возврат средств. [https://developers.cloudpayments.ru/#vozvrat-deneg]
 type RefundPaymentRequest struct {
 	TransactionId int64                  `json:"TransactionId"`
 	Amount        float64                `json:"Amount"`
@@ -58,32 +58,32 @@ type RefundPaymentRequest struct {
 	RequestID     *string                `json:"-"`
 }
 
-// VoidPaymentRequest представляет запрос на отмену оплаты.
+// VoidPaymentRequest представляет запрос на отмену оплаты. [https://developers.cloudpayments.ru/#otmena-oplaty]
 type VoidPaymentRequest struct {
 	TransactionId int64   `json:"TransactionId"`
 	RequestID     *string `json:"-"`
 }
 
-// GetPaymentRequest представляет запрос на получение информации о платеже.
+// GetPaymentRequest представляет запрос на получение информации о платеже. [[https://developers.cloudpayments.ru/#prosmotr-tranzaktsii]]
 type GetPaymentRequest struct {
 	TransactionId int64   `json:"TransactionId"`
 	RequestID     *string `json:"-"`
 }
 
-// FindPaymentRequest представляет запрос на поиск платежа по идентификатору счета.
+// FindPaymentRequest представляет запрос на поиск платежа по идентификатору счета. [https://developers.cloudpayments.ru/#proverka-statusa-platezha]
 type FindPaymentRequest struct {
 	InvoiceId string  `json:"InvoiceId"`
 	RequestID *string `json:"-"`
 }
 
-// PaymentsListRequest представляет запрос на получение списка транзакций за день.
+// PaymentsListRequest представляет запрос на получение списка транзакций за день. [https://developers.cloudpayments.ru/#vygruzka-spiska-tranzaktsiy]
 type PaymentsListRequest struct {
 	Date      string  `json:"Date"`
 	TimeZone  string  `json:"TimeZone,omitempty"`
 	RequestID *string `json:"-"`
 }
 
-// PaymentsListV2Request представляет запрос на получение списка транзакций за произвольный период.
+// PaymentsListV2Request представляет запрос на получение списка транзакций за произвольный период. [https://developers.cloudpayments.ru/#vygruzka-spiska-tranzaktsiy-za-proizvolnyy-period]
 type PaymentsListV2Request struct {
 	CreatedDateGte string   `json:"CreatedDateGte"`
 	CreatedDateLte string   `json:"CreatedDateLte"`
@@ -93,13 +93,13 @@ type PaymentsListV2Request struct {
 	RequestID      *string  `json:"-"`
 }
 
-// TokensListRequest представляет запрос на получение списка токенов.
+// TokensListRequest представляет запрос на получение списка токенов. [https://developers.cloudpayments.ru/#vygruzka-tokenov]
 type TokensListRequest struct {
 	PageNumber int     `json:"PageNumber"`
 	RequestID  *string `json:"-"`
 }
 
-// LinkPaymentRequest представляет запрос на создание заказа для отправки по email.
+// LinkPaymentRequest представляет запрос на создание заказа для отправки по email. [https://developers.cloudpayments.ru/#sozdanie-scheta-dlya-otpravki-po-pochte]
 type LinkPaymentRequest struct {
 	Amount               float64                `json:"Amount"`
 	Currency             *CurrencyCode          `json:"Currency"`
@@ -121,13 +121,13 @@ type LinkPaymentRequest struct {
 	RequestID            *string                `json:"-"`
 }
 
-// CancelOrderRequest представляет запрос на отмену заказа.
+// CancelOrderRequest представляет запрос на отмену заказа. [https://developers.cloudpayments.ru/#otmena-sozdannogo-scheta]
 type CancelOrderRequest struct {
 	Id        string  `json:"Id"`
 	RequestID *string `json:"-"`
 }
 
-// SubscriptionCreateRequest представляет запрос на создание подписки.
+// SubscriptionCreateRequest представляет запрос на создание подписки. [https://developers.cloudpayments.ru/#sozdanie-podpiski-na-rekurrentnye-platezhi]
 type SubscriptionCreateRequest struct {
 	Token               string                 `json:"Token"`
 	AccountId           string                 `json:"AccountId"`
@@ -144,7 +144,7 @@ type SubscriptionCreateRequest struct {
 	RequestID           *string                `json:"-"`
 }
 
-// SubscriptionUpdateRequest представляет запрос на обновление подписки.
+// SubscriptionUpdateRequest представляет запрос на обновление подписки. [https://developers.cloudpayments.ru/#izmenenie-podpiski-na-rekurrentnye-platezhi]
 type SubscriptionUpdateRequest struct {
 	Id                  string                 `json:"Id"`
 	Description         string                 `json:"Description,omitempty"`
@@ -160,25 +160,25 @@ type SubscriptionUpdateRequest struct {
 	RequestID           *string                `json:"-"`
 }
 
-// SubscriptionGetRequest представляет запрос на получение информации о подписке.
+// SubscriptionGetRequest представляет запрос на получение информации о подписке. [https://developers.cloudpayments.ru/#zapros-informatsii-o-podpiske]
 type SubscriptionGetRequest struct {
 	Id        string  `json:"Id"`
 	RequestID *string `json:"-"`
 }
 
-// SubscriptionCancelRequest представляет запрос на отмену подписки.
+// SubscriptionCancelRequest представляет запрос на отмену подписки. [https://developers.cloudpayments.ru/#otmena-podpiski-na-rekurrentnye-platezhi]
 type SubscriptionCancelRequest struct {
 	Id        string  `json:"Id"`
 	RequestID *string `json:"-"`
 }
 
-// SubscriptionListRequest представляет запрос на получение списка подписок.
+// SubscriptionListRequest представляет запрос на получение списка подписок. [https://developers.cloudpayments.ru/#poisk-podpisok]
 type SubscriptionListRequest struct {
 	AccountId string  `json:"AccountId"`
 	RequestID *string `json:"-"`
 }
 
-// CryptogramPayoutRequest представляет запрос на выплату по криптограмме.
+// CryptogramPayoutRequest представляет запрос на выплату по криптограмме. [https://developers.cloudpayments.ru/#vyplata-po-kriptogramme]
 type CryptogramPayoutRequest struct {
 	CardCryptogramPacket string                 `json:"CardCryptogramPacket"`
 	Amount               float64                `json:"Amount"`
@@ -194,7 +194,7 @@ type CryptogramPayoutRequest struct {
 	RequestID            *string                `json:"-"`
 }
 
-// TokenPayoutRequest представляет запрос на выплату по токену.
+// TokenPayoutRequest представляет запрос на выплату по токену. [https://developers.cloudpayments.ru/#vyplata-po-tokenu]
 type TokenPayoutRequest struct {
 	Token     string        `json:"Token"`
 	Amount    float64       `json:"Amount"`
